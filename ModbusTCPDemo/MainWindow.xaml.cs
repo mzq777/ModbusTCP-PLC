7158d4fe4b13b957b30662177f98717a35dc19fa
mzq777 <1033628976@qq.com>
2019-09-12 14:04:49 +0800

    Update MainWindow.xaml.cs

--- ModbusTCPDemo/MainWindow.xaml.cs
+++ ModbusTCPDemo/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HslCommunication;
 using HslCommunication.ModBus;
 
 namespace ModbusTCPDemo
@@ -23,6 +10,9 @@ namespace ModbusTCPDemo
     public partial class MainWindow : Window
     {
         private ModbusTcpNet busTcpClient;
+
+        public ModbusTcpNet BusTcpClient { get => busTcpClient; set => busTcpClient = value; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,10 +24,10 @@ namespace ModbusTCPDemo
         /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            busTcpClient = new ModbusTcpNet(this.txtIP.Text, Int32.Parse(this.txtPort.Text));
+            BusTcpClient = new ModbusTcpNet(this.txtIP.Text, Int32.Parse(this.txtPort.Text));
             //在Modbus服务器的设备里，大部分的设备都是从地址0开始的，有些特殊的设备是从地址1开始的，所以本组件里面，默认从地址0开始，如果想要从地址1开始，那么就需要如下的配置：
-            busTcpClient.AddressStartWithZero = false;
-            busTcpClient.ConnectServer();
+            BusTcpClient.AddressStartWithZero = false;
+            BusTcpClient.ConnectServer();
             MessageBox.Show("连接成功！");
         }
         /// <summary>
@@ -47,7 +37,7 @@ namespace ModbusTCPDemo
         /// <param name="e"></param>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            busTcpClient.ConnectClose();
+            BusTcpClient.ConnectClose();
             MessageBox.Show("断开连接成功！");
         }
         /// <summary>
@@ -57,7 +47,7 @@ namespace ModbusTCPDemo
         /// <param name="e"></param>
         private void BtnSet_Click(object sender, RoutedEventArgs e)
         {
-            busTcpClient.Write(this.txtSetAddress.Text, Int32.Parse(this.txtSetVal.Text));
+            BusTcpClient.Write(this.txtSetAddress.Text, Int32.Parse(this.txtSetVal.Text));
             MessageBox.Show("设置成功！");
         }
         /// <summary>
@@ -67,7 +57,7 @@ namespace ModbusTCPDemo
         /// <param name="e"></param>
         private void BtnRead_Click(object sender, RoutedEventArgs e)
         {
-            this.txtReadVal.Text = busTcpClient.ReadInt32Async(this.txtReadAddress.Text).Result.Content.ToString();            
+            this.txtReadVal.Text = BusTcpClient.ReadInt32Async(this.txtReadAddress.Text).Result.Content.ToString();            
         }
 
     }
